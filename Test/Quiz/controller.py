7d25d56a6e0a6f1aa929eebaee8fcda3d2d07ba7
thediegup <diegodepedro1@gmail.com>
2022-04-12 20:01:52 -0400

    ampliado las opciones de quiz a 4, y otras cosas

--- Test/Quiz/controller.py
+++ Test/Quiz/controller.py
@@ -15,14 +15,16 @@ class Question:
     option1 = ""
     option2 = ""
     option3 = ""
+    option4 = ""
     correctOption = -1
 
-    def __init__(self,q_id,question,option1,option2,option3,correctOption):
+    def __init__(self,q_id,question,option1,option2,option3,option4,correctOption):
         self.q_id = q_id
         self.question = question
         self.option1 = option1
         self.option2 = option2
         self.option3 = option3
+        self.option4 = option4
         self.correctOption = correctOption
 
     def get_correct_option(self):
@@ -32,12 +34,21 @@ class Question:
             return self.option2
         elif self.correctOption == 3:
             return self.option3
+        elif self.correctOption == 4:
+            return self.option4
 
-q1 = Question(1, "¿Cuál es el nombre de tu universidad?","UCAB","UET","UCV",1)
-q2 = Question(2, "¿Donde esta la universidad ubicada?","Bolivia","Mexico","Caracas",3)
-q3 = Question(3, "¿Cuál es el nombre de tu carrera?","Comunicación","Ingeniería","Psicología",2)
-q4 = Question(4, "¿Quien fue el profesor de Simon Bolivar?","Andrés Bello","George Washington","Lincoln",1)
-questions_list = [q1,q2,q3,q4]
+qa1 = Question(1, "¿Cuál es el nombre de tu universidad?","UCAB","UET","UCV","MIT",1)
+qa2 = Question(2, "¿Donde esta la universidad ubicada?","Bolivia","Mexico","Caracas","Boston",3)
+qa3 = Question(3, "¿Cuál es el nombre de tu carrera?","Comunicación","Ingeniería","Psicología","Medicina",2)
+qa4 = Question(4, "¿Quien fue el profesor de Simon Bolivar?","Andrés Bello","George Washington","Lincoln","Ana Maria",1)
+questions_list1 = [qa1,qa2,qa3,qa4]
+qb1 = Question(1,"¿Quien fue el primer presidente de Estados Unidos?","Lincoln","Washington","Jefferson","Roosevelt",2)
+qb2 = Question(2,"¿Cuantos estados tiene Estados Unidos?","48","52","46","50",4)
+qb3 = Question(3,"¿Cual de las siguientes comidas NO es un producto lacteo?","Leche","Hamburguesa","Yogur","Queso",2)
+qb4 = Question(4,"¿Cuál es un pais Sudamericano?","Rusia","Argentina","Mexico","Japón",2)
+questions_list2 = [qb1,qb2,qb3,qb4]
+
+questions_list = questions_list1
 
 @app.route("/main", methods=["POST","GET"])
 def main():
@@ -49,7 +60,15 @@ def main():
         pic6 = os.path.join(app.config['UPLOAD_FOLDER'],'historia.svg')
         pic7 = os.path.join(app.config['UPLOAD_FOLDER'],'banner2.jpg')
         if request.method == "POST":
-            return redirect(url_for('quiz'))
+            if request.form['submit_button'] == 'castellano':
+                return redirect(url_for('quiz', questions_list = questions_list1)) #Chequear pagina de las pestañas para solucionar error de paso de lista (redirect list)
+            
+            elif request.form['submit_button'] == 'matematica':
+                return redirect(url_for('quiz', questions_list = questions_list2))
+            
+            else:
+                return redirect(url_for(main)),404
+
         else:
             return render_template("index.html", 
                                 user_image1 = pic1, 
@@ -61,7 +80,7 @@ def main():
                                 user_image7 = pic7)
         
 
-@app.route("/quiz")
+@app.route("/quiz", methods = ['POST','GET'])
 def quiz():
         return render_template("quiz.html", questions_list = questions_list)
 
